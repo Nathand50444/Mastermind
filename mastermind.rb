306723805ba0427295c77453b2c3005d6e0a9d5a
NathanD <nathand50444@gmail.com>
2024-03-19 21:53:21 +0000

    Method adds 'code_guess' onto the game board
    Multiple methods created to add the 'code_guess',
    and later the feedback, to the game board.

--- mastermind.rb
+++ mastermind.rb
@@ -2,43 +2,71 @@ class Mastermind
 
     def initialize
         @board = Array.new(96) { " " }
-        codemaker_code
+        show_board
     end
 
     def show_board
         puts "---------------------------------"
-        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} | #{@board[3]} | | | #{@board[4]} | #{@board[5]} | #{@board[6]} | #{@board[7]}"
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} | #{@board[3]} | | | #{@board[4]} | #{@board[5]} | #{@board[6]} | #{@board[7]} |"
         puts "---------------------------------"
-        puts " #{@board[8]} | #{@board[9]} | #{@board[10]} | #{@board[11]} | | | #{@board[12]} | #{@board[13]} | #{@board[14]} | #{@board[15]}" 
+        puts " #{@board[8]} | #{@board[9]} | #{@board[10]} | #{@board[11]} | | | #{@board[12]} | #{@board[13]} | #{@board[14]} | #{@board[15]} |" 
         puts "---------------------------------"
-        puts " #{@board[16]} | #{@board[17]} | #{@board[18]} | #{@board[19]} | | | #{@board[20]} | #{@board[21]} | #{@board[22]} | #{@board[23]}" 
+        puts " #{@board[16]} | #{@board[17]} | #{@board[18]} | #{@board[19]} | | | #{@board[20]} | #{@board[21]} | #{@board[22]} | #{@board[23]} |" 
         puts "---------------------------------"
-        puts " #{@board[24]} | #{@board[25]} | #{@board[26]} | #{@board[27]} | | | #{@board[28]} | #{@board[29]} | #{@board[30]} | #{@board[31]}" 
+        puts " #{@board[24]} | #{@board[25]} | #{@board[26]} | #{@board[27]} | | | #{@board[28]} | #{@board[29]} | #{@board[30]} | #{@board[31]} |" 
         puts "---------------------------------"
-        puts " #{@board[32]} | #{@board[33]} | #{@board[34]} | #{@board[35]} | | | #{@board[36]} | #{@board[37]} | #{@board[38]} | #{@board[39]}"
+        puts " #{@board[32]} | #{@board[33]} | #{@board[34]} | #{@board[35]} | | | #{@board[36]} | #{@board[37]} | #{@board[38]} | #{@board[39]} |"
         puts "---------------------------------"
-        puts " #{@board[40]} | #{@board[41]} | #{@board[42]} | #{@board[43]} | | | #{@board[44]} | #{@board[45]} | #{@board[46]} | #{@board[47]}" 
+        puts " #{@board[40]} | #{@board[41]} | #{@board[42]} | #{@board[43]} | | | #{@board[44]} | #{@board[45]} | #{@board[46]} | #{@board[47]} |" 
         puts "---------------------------------"
-        puts " #{@board[48]} | #{@board[49]} | #{@board[50]} | #{@board[51]} | | | #{@board[52]} | #{@board[53]} | #{@board[54]} | #{@board[55]}" 
+        puts " #{@board[48]} | #{@board[49]} | #{@board[50]} | #{@board[51]} | | | #{@board[52]} | #{@board[53]} | #{@board[54]} | #{@board[55]} |" 
         puts "---------------------------------"
-        puts " #{@board[56]} | #{@board[57]} | #{@board[58]} | #{@board[59]} | | | #{@board[60]} | #{@board[61]} | #{@board[62]} | #{@board[63]}" 
+        puts " #{@board[56]} | #{@board[57]} | #{@board[58]} | #{@board[59]} | | | #{@board[60]} | #{@board[61]} | #{@board[62]} | #{@board[63]} |" 
         puts "---------------------------------"
-        puts " #{@board[64]} | #{@board[65]} | #{@board[66]} | #{@board[67]} | | | #{@board[68]} | #{@board[69]} | #{@board[70]} | #{@board[71]}" 
+        puts " #{@board[64]} | #{@board[65]} | #{@board[66]} | #{@board[67]} | | | #{@board[68]} | #{@board[69]} | #{@board[70]} | #{@board[71]} |" 
         puts "---------------------------------"
-        puts " #{@board[72]} | #{@board[73]} | #{@board[74]} | #{@board[75]} | | | #{@board[76]} | #{@board[77]} | #{@board[78]} | #{@board[79]}" 
+        puts " #{@board[72]} | #{@board[73]} | #{@board[74]} | #{@board[75]} | | | #{@board[76]} | #{@board[77]} | #{@board[78]} | #{@board[79]} |" 
         puts "---------------------------------"
-        puts " #{@board[80]} | #{@board[81]} | #{@board[82]} | #{@board[83]} | | | #{@board[84]} | #{@board[85]} | #{@board[86]} | #{@board[87]}" 
+        puts " #{@board[80]} | #{@board[81]} | #{@board[82]} | #{@board[83]} | | | #{@board[84]} | #{@board[85]} | #{@board[86]} | #{@board[87]} |" 
         puts "---------------------------------"
-        puts " #{@board[88]} | #{@board[89]} | #{@board[90]} | #{@board[91]} | | | #{@board[92]} | #{@board[93]} | #{@board[94]} | #{@board[95]}"  
+        puts " #{@board[88]} | #{@board[89]} | #{@board[90]} | #{@board[91]} | | | #{@board[92]} | #{@board[93]} | #{@board[94]} | #{@board[95]} |"  
     end
 
     def turn_count
-        # Concatenate 1 each time a 'turn' is called?
+        turn_count = 0
+        
     end
 
     def codemaker_code
         puts "You are the codemaker! Please choose your super secret, 4-digit code. (e.g 1234)"
-        code = gets.chomp.to_i
+        code_to_break = gets.chomp.chars.map(&:to_i)
     end
 
-    def
+    def codebreaker_guess
+        puts "You are the codebreaker! Guess the 4 digit code."
+        code_guess = gets.chomp.chars.map(&:to_i)
+    end 
+
+    def add_to_gameboard(game_turn, code_guess)
+        box_index_1 = 0 + (game_turn * 8)
+        box_index_2 = 1 + (game_turn * 8)
+        box_index_3 = 2 + (game_turn * 8)
+        box_index_4 = 3 + (game_turn * 8)
+        @board[box_index_1] = code_guess[1]
+        @board[box_index_2] = code_guess[2]
+        @board[box_index_3] = code_guess[3]
+        @board[box_index_4] = code_guess[4]                     # Four lots of these (+1, +2, +3, +4) for each of code_guess[0 to 4]
+    end
+
+    def add_to_feedback
+        fb_index_1 = 4 + (game_turn * 8)
+        fb_index_2 = 5 + (game_turn * 8)
+        fb_index_3 = 6 + (game_turn * 8)
+        fb_index_4 = 7 + (game_turn * 8)
+        # This will add in the feedback sections.
+    end
+    
+    def feedback(code_guess)
+        
+    end
+end
